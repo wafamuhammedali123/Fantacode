52d0437eefccfe2bc99da980431425698be1bfe3
wafamuhammedali123 <33367269+wafamuhammedali123@users.noreply.github.com>
2017-12-15 04:00:00 -0800

    MiddleWare
    MiddleWare is created

---
+++ MiddleWare/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+public class Startup
+{
+    private static void HandleMapTest1(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Map Test 1");
+            Console.WriteLine("abc");
+        });
+    }
+
+    private static void HandleMapTest2(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Map Test 2");
+            Console.WriteLine("def");
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.Map("/map1", HandleMapTest1);
+
+        app.Map("/map2", HandleMapTest2);
+
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Hello from non-Map delegate. <p>");
+        });
+    }
+}
